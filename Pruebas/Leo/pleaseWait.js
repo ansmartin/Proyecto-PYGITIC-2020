ca70e0ecbd5a51ea1a5fca066fef7a15612bace0
denibel2507 <61326303+denibel2507@users.noreply.github.com>
2020-05-24 14:04:09 +0200

    Front-end actualizado

---
+++ Pruebas/Leo/pleaseWait.js
@@ -0,0 +1,33 @@
+/**
+ * Displays overlay with "Please wait" text. Based on bootstrap modal. Contains animated progress bar.
+ */
+function showPleaseWait() {    
+    if (document.querySelector("#pleaseWaitDialog") == null) {
+        var modalLoading = '<div class="modal" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false" role="dialog">\
+            <div class="modal-dialog">\
+                <div class="modal-content">\
+                    <div class="modal-header">\
+                        <h4 class="modal-title">Please wait...</h4>\
+                    </div>\
+                    <div class="modal-body">\
+                        <div class="progress">\
+                          <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar"\
+                          aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width:100%; height: 40px">\
+                          </div>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>';
+        $(document.body).append(modalLoading);
+    }
+  
+    $("#pleaseWaitDialog").modal("show");
+}
+
+/**
+ * Hides "Please wait" overlay. See function showPleaseWait().
+ */
+function hidePleaseWait() {
+    $("#pleaseWaitDialog").modal("hide");
+}
