6cb84dee0ef04de4e79456a844dd25f1db5aa1ee
AMM <anselmm95@gmail.com>
2020-05-27 12:04:42 +0200

    lambdas actualizadas para enviar y leer json

--- Pruebas/Ansel/temp_to_topic.py
+++ Pruebas/Ansel/temp_to_topic.py
@@ -1,19 +1,28 @@
 import boto3
 import random
+import json
 
 topic = 'arn:aws:sns:us-east-1:511086078676:temps'
 client = boto3.client('sns')
 
 def lambda_handler(event, context):
     
+    #id de la persona (editar con el id real)
+    id = '11223344'
+    
     #simulador de aparato de medir la temperatura
     temp = random.uniform(36,40)
     print(temp)
     
+    mensaje = {
+        'id' : id,
+        'temp' : '%.2f' % round(temp, 2)
+    }
+    
     #publica mensaje en el topic
     response = client.publish(
         TopicArn = topic,
-        Message = "%.2f" % round(temp, 2)
+        Message = json.dumps(mensaje)
         )
         
     print(response)
--- Pruebas/Ansel/topic_to_bbdd.py
+++ Pruebas/Ansel/topic_to_bbdd.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import decimal
+import json
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 tabla = dynamodb.Table('temperaturas')
@@ -11,13 +12,18 @@ def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
     
     m = event['Records'][0]['Sns']
-    temp = decimal.Decimal(m['Message'])
+    mensaje = json.loads(m['Message'])
     
-    print("Recibido: ", temp)
-    print("Tipo del mensaje: ", type(temp))
+    print("JSON recibido: ", mensaje)
+    
+    temp = decimal.Decimal(mensaje['temp']) #decimal.Decimal(m['Message'])
+    
+    print("Temperatura recibida: ", temp)
+    print("Tipo de dato temperatura: ", type(temp))
     
     tabla.put_item(Item={
         'id' : m['MessageId'],
         'date' : m['Timestamp'],
+        'userid' : mensaje['id'],
         'temp' : temp
     })
