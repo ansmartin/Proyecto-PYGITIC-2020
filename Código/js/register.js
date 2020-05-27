f845ebacf10e9bc30cc1bd44aeaec9ba70eed1f0
Leonardo Cardozo Gómez <Leonardo.Cardozo@alu.uclm.es>
2020-05-27 18:48:42 +0200

    Funcionalidades contraseña olvidada y cambiar contraseña con confirmación de código de contraseña olvidada revisadas, funcionan correctamente, aunque la transición entre ellas (onSuccess) falla.

--- Código/js/register.js
+++ Código/js/register.js
@@ -5,38 +5,9 @@ window._config = {
         region: 'us-east-1'
     },
 	api: {
-        invokeUrl: 'https://4hd9ckubxi.execute-api.us-east-1.amazonaws.com/prod'	//'https://kirm7lqi9d.execute-api.us-east-1.amazonaws.com/prod'
+        invokeUrl: 'https://4hd9ckubxi.execute-api.us-east-1.amazonaws.com/prod'
     }
 };
-
-
-/*
-var outputA
-var statusA
-var xhrA
-var aux = $.ajax({
-			method: 'GET',
-			aysnc: false,
-			url: "https://4hd9ckubxi.execute-api.us-east-1.amazonaws.com/prod/heatsense",
-            headers: {
-                'Authorization': "eyJraWQiOiJjWXN6bmJuaVBNcm5qdytFcHdZQTN2ZTdnSzM1Q1Y4K0F2dFBVc293ODZNPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJkNTY1NGE5OC1kYTllLTQxYmUtOGNjOC1lY2VjYmU2NGQxNjMiLCJhdWQiOiI3ZGUyMWxuNTFsMW85OTV2NWZxMGlwYzF2aCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJldmVudF9pZCI6ImYxOTQxOGQ3LTk3Y2MtNDgxNy1iZDA2LTAzMzFhMWIwZTcxMyIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNTkwNTIzMjg5LCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9TUGhGNm05NWoiLCJjb2duaXRvOnVzZXJuYW1lIjoibGVvbmFyZG8uY2FyZG96b0BhbHUudWNsbS5lcyIsImV4cCI6MTU5MDUyNjg4OSwiaWF0IjoxNTkwNTIzMjg5LCJlbWFpbCI6Imxlb25hcmRvLmNhcmRvem9AYWx1LnVjbG0uZXMifQ.Ey2KNiJcBaYbcAt92VP2QgUQNIAo9dbE0kfDglDLk6B6Mg1i15bXnbwEKZIa17fwUIn9GIx8fwHU4cyq728iPn7m_Y_a8Vo1TzYB0hyJB5l2hoDJp4XkQc2whYM1BKWrl_QU_B_24meSUGw2nPQmjndXKPzfts1SIURlcU3MOhU67Tr5JV2i4-TSfBw4oP_rMG2RY8ogVapKHWALS8hv6tDq3zxyr-9JnIg5MyeRfqZZsveSLjSJ7m7MLRnD77UQhQkYn2ph7lHjAQFUzPzLVAqiPwr2ZfVtvODUaLqabnSoQ5pOzp8QOmXA8VvBAmyxjTkb-t_3lGcKOfD26HGwBQ",
-				'Access-Control-Allow-Origin': '*'
-            },
-			success: function(output, status, xhr){
-				console.log("All: " + xhr.getAllResponseHeaders());
-				console.log("output" + output);
-			},
-			error: function(output, status, xhr){
-				console.log("Error: " + status);
-				outputA = output
-				statusA = status
-				xhrA = xhr
-				//console.log("Error output: " + type(output));
-				//console.log("Error All: " + json.stringify(xhr));
-			}
-		})
-*/
-
 
 var HeatSense = window.HeatSense || {};
 
@@ -121,8 +92,8 @@ var HeatSense = window.HeatSense || {};
         });
     }
 	
-	/*function forgot(email, onSuccess, onFailure) {
-        createCognitoUser(email).forgotPassword(function confirmCallback(err, result) {
+	function forgot(email, onSuccess, onFailure) {
+        createCognitoUser(email).forgotPassword(email, function confirmCallback(err, result) {
             if (!err) {
                 onSuccess(result);
             } else {
@@ -131,15 +102,15 @@ var HeatSense = window.HeatSense || {};
         });
     }
 	
-	function confirmForgotPassword(code, email, password) {
-        createCognitoUser(email).confirmForgotPassword(code, password, function confirmCallback(err, result) {
+	function confirmForgotPassword(code, email, password, onSuccess, onFailure) {
+        createCognitoUser(email).confirmPassword(code, password, function confirmCallback(err, result) {
             if (!err) {
                 onSuccess(result);
             } else {
                 onFailure(err);
             }
         });
-    }*/
+    }
 
     function createCognitoUser(email) {
         return new AmazonCognitoIdentity.CognitoUser({
@@ -155,7 +126,8 @@ var HeatSense = window.HeatSense || {};
         $('#signinForm').submit(handleSignin);
         $('#registerForm').submit(handleRegister);
         $('#verifyForm').submit(handleVerify);
-		//$('#forgotForm').submit(handleForgot);
+		$('#forgotForm').submit(handleForgot);
+		$('#confirmForgotForm').submit(handleConfirmForgot);
     });
 	
 	function getTemp(){
@@ -165,26 +137,13 @@ var HeatSense = window.HeatSense || {};
                 Authorization: authToken
             };
 		
-		/*var client = new XMLHttpRequest();
-		client.open("GET", url, true);
-		client.setRequestHeader('Authorization', authToken);
-		client.setRequestHeader('Access-Control-Allow-Origin', '*');
-		client.send();
-		client.onreadystatechange = function() {
-			console.log("All?: " + client.getAllResponseHeaders());
-			console.log("Headers: " + client.HEADERS_RECEIVED)
-		  /*if(this.readyState == this.HEADERS_RECEIVED) {
-			console.log("log: " + this.getAllResponseHeaders());
-		  }
-		}*/
-		
 		$.ajax({
 			method: 'GET',
 			async: false,
 			url: url,
             headers: {
                 'Authorization': authToken,
-				'Access-Control-Allow-Origin': '*'//'http://muii-pygitic-abp-heatsense.s3-website-us-east-1.amazonaws.com/'
+				'Access-Control-Allow-Origin': '*'
             },
 			success: function(output, status, xhr){
 				datos = xhr.getResponseHeader("content-type");
@@ -244,7 +203,6 @@ var HeatSense = window.HeatSense || {};
 
     function handleVerify(event) {
         var email = $('#emailVerifyForm').val();
-		//var email = (new URLSearchParams(window.location.search)).searchParams.get('user');
 		var code = $('#codeVerifyForm').val();
 		var onSuccess = function verifySuccess(result) {
 			console.log('call result: ' + result);
@@ -259,27 +217,27 @@ var HeatSense = window.HeatSense || {};
 		verify(email, code, onSuccess, onFailure);
     }
 	
-	/*function handleForgot(event) {
-        var email = $('#email').val();
+	function handleForgot(event) {
+        var email = $('#emailForgot').val();
         event.preventDefault();
         forgot(email,
             function forgotSuccess(result) {
                 console.log('call result: ' + result);
                 console.log('Successfully forgot');
-                alert('Forgot successful. You will now be redirected to the login page.');
-                window.location.href = signinUrl;
+                alert('Forgot successful. You will now be redirected to the confirm code forgot page.');
+                window.location.href = "confirmForgot.html";
             },
             function forgotError(err) {
                 alert(err);
             }
         );
-    }*/
+    }
 	
-	/*function handleConfirmForgot(event) {
+	function handleConfirmForgot(event) {
 		var code = $('#codeInputConfirmForgot').val();
-        var email = $('#usernameInputConfirmForgot').val();
-        var password = $('#passwordInputRegister').val();
-        var password2 = $('#password2InputRegister').val();
+        var email = $('#emailInputConfirmForgot').val();
+        var password = $('#passwordInputConfirmForgot').val();
+        var password2 = $('#password2InputConfirmForgot').val();
         event.preventDefault();
         if (password === password2) {
             confirmForgotPassword(code, email, password, 
@@ -296,5 +254,5 @@ var HeatSense = window.HeatSense || {};
         } else {
             alert('Passwords do not match');
         }
-    }*/
+    }
 }(jQuery));
